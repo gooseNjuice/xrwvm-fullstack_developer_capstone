1ccc2cd68b81d78491b27ec0eda058ecd7f2a304
goosenjuice <zamirkononov@gmail.com>
2025-04-25 04:18:55 -0400

    tralala

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,6 +62,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
             os.path.join(BASE_DIR,'frontend/static')
         ],
         'APP_DIRS': True,
@@ -137,6 +139,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
